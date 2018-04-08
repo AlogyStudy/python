e0e2ca06aeffe1224933768c872e4b51217e9895
alogy <1129507496@qq.com>
2018-04-08 23:02:11 +0800

    add O(1)

--- code/algorithm/for.py
+++ code/algorithm/for.py
@@ -3,4 +3,15 @@
 # n ^ 2 -> 双重for循环
 
 
+# for a in range(0, n):
+#     for b in range(0, n):
+#         c = 1000 - a - b
+#         if a ** 2 + b ** 2 == c **2:
+#             print('a, b, c为：%d'%(a, b, c))
 
+# T(1000) = 1000 * 1000 * (1 + 1)
+# T(n) = n * n * (1 + max(1, 0))
+#  = n ^ 2 * 2
+#  = O(n ^ 2)
+# 
+ 
