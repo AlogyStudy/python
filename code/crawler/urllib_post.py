36f21186b7cce85638995ab23a353f5481e19552
alogy <1129507496@qq.com>
2018-06-04 23:59:23 +0800

    add urllib post

---
+++ code/crawler/urllib_post.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+
+import urllib.request
+import urllib.parse
+
+fromdata = [
+    'i': 'python'
+    'from': 'AUTO'
+    'to': 'AUTO'
+    'smartresult': 'dict'
+    'client': 'fanyideskweb'
+    'salt': '1528127663128'
+    'sign': 'ab3f8ec2a48bf8a1004213fff0c24912'
+    'doctype': 'json'
+    'version': '2.1'
+    'keyfrom': 'fanyi.web'
+    'action': 'FY_BY_REALTIME'
+    'typoResult': 'false'
+]
