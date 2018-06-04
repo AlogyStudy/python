1940dcb5a0b42c167f3c4784121219a36244f597
alogy <1129507496@qq.com>
2018-06-05 00:21:05 +0800

    change urlib post

--- code/crawler/urllib_post.py
+++ code/crawler/urllib_post.py
@@ -3,17 +3,34 @@
 import urllib.request
 import urllib.parse
 
-fromdata = [
-    'i': 'python'
-    'from': 'AUTO'
-    'to': 'AUTO'
-    'smartresult': 'dict'
-    'client': 'fanyideskweb'
-    'salt': '1528127663128'
-    'sign': 'ab3f8ec2a48bf8a1004213fff0c24912'
-    'doctype': 'json'
-    'version': '2.1'
-    'keyfrom': 'fanyi.web'
-    'action': 'FY_BY_REALTIME'
+url = 'http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule'
+
+key = input('请输入查询翻译的文字：')
+
+# 发送到服务器的表单数据，如果是中文需要转码
+fromdata = {
+    'i': key,
+    'from': 'AUTO',
+    'to': 'AUTO',
+    'smartresult': 'dict',
+    'client': 'fanyideskweb',
+    'salt': '1528127663128',
+    'sign': 'c060b56b628f82259225f751c12da59a',
+    'doctype': 'json',
+    'version': '2.1',
+    'keyfrom': 'fanyi.web',
+    'action': 'FY_BY_REALTIME',
     'typoResult': 'false'
-]
+}
+
+data = urllib.parse.urlencode(fromdata).encode('utf-8')
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
+}
+
+request = urllib.request.Request(url, data=data, headers=headers)
+
+html = urllib.request.urlopen(request).read().decode()
+
+print(html)
