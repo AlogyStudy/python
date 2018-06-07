6a56bd37600e96989c7921e6f0205cccf26a5ee9
alogy <1129507496@qq.com>
2018-06-08 00:10:27 +0800

    change urlopenhadnler

--- code/crawler/urllib_urlopenhandler.py
+++ code/crawler/urllib_urlopenhandler.py
@@ -3,7 +3,8 @@
 import urllib.request
 
 # 构建一个HTTPHandler处理器对象，支持处理HTTP的请求
-http_hander = urllib.request.HTTPHandler()
+# http_hander = urllib.request.HTTPHandler()
+http_hander = urllib.request.HTTPHandler(debuglevel=1)
 
 # 调用build_opener()方法构建一个自定义的opener对象，参数是构建的处理器对象
 opener = urllib.request.build_opener(http_hander)
