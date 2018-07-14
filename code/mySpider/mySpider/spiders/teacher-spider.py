0414611adc9d409d546fbcfdab63359421143f16
alogy <1129507496@qq.com>
2018-07-14 14:03:34 +0800

    scrapy study

--- code/mySpider/mySpider/spiders/teacher-spider.py
+++ code/mySpider/mySpider/spiders/teacher-spider.py
@@ -1,15 +1,35 @@
 #coding=utf-8
 
 import scrapy
+from mySpider.items import ItItems
 
 class TeSpider(scrapy.Spider):
     # 爬虫名字
-    name = 'te'
-    # 允许爬虫作用的范围
-    allowdDomains = ['http://www.itcast.cn/']
-    # 爬虫起始的url
-    startUrls = ['http://www.itcast.cn/channel/teacher.shtml#']
+    name = 'ing'
+    # 允许爬虫作用的范围 固定属性名allowed_domains
+    allowed_domains = ['http://www.itcast.cn/']
+    # 爬虫起始的url 固定属性名start_urls
+    start_urls = ['http://www.itcast.cn/channel/teacher.shtml']
 
     def parse(self, response):
-        with open('teacher.html', 'w') as f:
-            f.write(response.body.decode())
+        # with open('teacher.html', 'w') as f:
+        #     f.write(response.body.decode())
+
+        tearcherList = response.xpath('//div[@class="li_txt"]')
+
+        itemTeachs = []
+        for item in tearcherList:
+            itemTeach = ItItems()
+            # name
+            name = item.xpath('./h3/text()').extract()
+            # title
+            title = item.xpath('./h4/text()').extract()
+            # info
+            info = item.xpath('./p/text()').extract()
+
+            itemTeach['name'] = name[0]
+            itemTeach['title'] = title[0]
+            itemTeach['info'] = info[0]
+            itemTeachs.append(itemTeach)
+
+        return itemTeachs
